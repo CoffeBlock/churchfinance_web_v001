40d0f2af5a5d3391b959db8338b6c4f7fe1466f0
CoffeBlock <coffedev1111@gmail.com>
2024-10-26 17:36:33 -0700

    modified:   package-lock.json 	modified:   package.json 	new file:   src/app/dashboard/page.tsx 	modified:   src/app/globals.css 	new file:   src/app/nav.tsx 	modified:   tailwind.config.ts

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,10 @@
+import Nav from "../nav";
+
+export default function Home() {
+  return (
+    <main className="flex flex-col bg-base-100" style={{minHeight: "100vh"}}>
+      <Nav />
+      
+    </main>
+  );
+}
